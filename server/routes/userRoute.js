5142cdcb414ca420eb3ea97dc42e05912b8ce7d3
Syed Laeeq Ahmed <laeeqe@icloud.com>
2024-03-14 00:41:41 +0500

    first commit

---
+++ server/routes/userRoute.js
@@ -0,0 +1,13 @@
+import express from "express"
+import { bookVisit, cancelBooking, createUser, getAllBookings, toFav } from "../controllers/userCntrl.js"
+
+
+const router = express.Router()
+
+router.post("/register", createUser)
+router.post("/bookVisit/:id", bookVisit)
+router.get("/allBookings", getAllBookings)
+router.post("/removeBooking/:id", cancelBooking)
+router.post("/toFav/:id", toFav)
+
+export {router as userRoute}
